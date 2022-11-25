7ee211385a06b9f3618efc7c0ddb77b548774a6d
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-11-25 07:11:13 +0530

    added string functions and constraints
    added string functions and constraints

--- sql-training.sql
+++ sql-training.sql
@@ -280,6 +280,7 @@ desc employee ;
  * unique
  * primary key 
  * default
+ *
  * */
 
 
@@ -359,7 +360,7 @@ desc person;
 
 # primary key ->  not null + unique
 
-
+#primary key -way 1
 drop table person;
 
 create table person(
@@ -377,6 +378,11 @@ insert into person values(2,'virat','kohli',40);
 
 select * from person;
 
+#primary key -way 2
+
+
+
+#default
 
 drop table person;
 
@@ -392,11 +398,257 @@ insert into person(id,firstName,lastName) values(2,'virat','kohli');
 select * from person;
 
 
-#MultiInsert
-#Constraints
+
+
+
+#auto_increment
+drop table person;
+
+create table person(
+	id int primary key auto_increment ,
+	firstName varchar(20) not null,
+	lastName varchar(20) not null,
+	age int not null default 20
+);
+
+insert into person(firstName,lastName,age) values('virat','kohli',45);
+
+insert into person(firstName,lastName,age) values('Rahul','Sharma',38);
+
+select * from person;
+
+
+#check
+drop table person;
+
+create table person(
+	id int primary key auto_increment ,
+	firstName varchar(20) not null,
+	lastName varchar(20) not null,
+	age int not null check (age>18)
+);
+
+insert into person(firstName,lastName,age) values('virat','kohli',20);
+
+insert into person(firstName,lastName,age) values('Rahul','Sharma',25);
+
+insert into person(firstName,lastName,age) values('Tejas','Varma',10);
+
+select * from person;
+
+
+
+
+
+#foreign key
+#composite key
 
 
 #String function 
+
+#refer belo link for more string function
+#https://dev.mysql.com/doc/refman/8.0/en/string-functions.html
+
+#Code formatter 
+#https://codebeautify.org/sqlformatter
+
+
+create database booksdb;
+
+use booksdb;
+
+CREATE TABLE books 
+	(
+		book_id INT AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
+ 
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+VALUES
+('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+
+select * from books b ;
+
+#concat
+SELECT CONCAT('pi', 'ckle');
+ 
+SELECT CONCAT(author_fname,' ', author_lname) AS author_name FROM books;
+
+SELECT CONCAT_WS('-', 'I', 'am', 'learning', 'SQL!');
+
+SELECT CONCAT_WS('-',title, author_fname, author_lname) FROM books;
+
+
+#substring
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+ 
+SELECT SUBSTRING('Hello World', -4);
+ 
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+
+
+#multi function
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+
+
+#Replace
+SELECT REPLACE('Hello World', 'Hell', '%$#@');
+ 
+SELECT REPLACE('Hello World', 'l', '7');
+ 
+SELECT REPLACE('Hello World', 'o', '0');
+ 
+SELECT REPLACE('HellO World', 'o', '*');
+ 
+select REPLACE('cheese bread coffee milk', ' ', ' and ');
+ 
+SELECT REPLACE(title, 'e ', '3') FROM books;
+ 
+SELECT REPLACE(title, ' ', '-') FROM books;
+
+
+#reverse
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
+
+
+#length
+SELECT CHAR_LENGTH('Hello World');
+ 
+SELECT CHAR_LENGTH(title) as length, title FROM books;
+ 
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+ 
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+
+#upper , lower
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
+
+
+
+#INSERT
+SELECT INSERT('Hello Bobby', 6, 0, 'There'); 
+
+SELECT INSERT('Hello Bobby', 6, 1, 'There'); 
+ 
+SELECT INSERT('Hello Bobby', 6, 2, 'There'); 
+
+#LEFT RIGHT REPEAT TRIM
+SELECT LEFT('omghahalol!', 3);
+ 
+SELECT RIGHT('omghahalol!', 4);
+ 
+SELECT REPEAT('ha', 4);
+ 
+SELECT TRIM('  pickle  ');
+
+
+#Excercise -1 
+reverse and uppercase the following sentence
+
+ #A set of words that is complete in itself
+
+replace spaces in titles with ->
+print name and reverse of the name
+print book title and length of each title
+print short title , author , quantity
+
+#Excercise-2
+CRUD Excercise
+---------------------
+create database - mycart
+
+create table cloaths
+
+cloathid(primary key)  , article , color  , size , price , available
+
+
+insert into cloaths
+('T-SHIRT' , 'BLUE' , 'M' , 2000,'Y');
+('T-SHIRT' , 'WHITE' , 'L' , 2500,'Y');
+('T-SHIRT' , 'RED' , 'S' , 1000,'N');
+('T-SHIRT' , 'ORANGE' , 'M' , 3000,'Y');
+('SHIRT' , 'WHITE' , 'M' , 5000,'Y');
+('SHIRT' , 'BLUE' , 'L' , 3500,'N');
+('SHIRT' , 'GREY' , 'M' , 3400,'Y');
+('TANK-TOP' , 'WHITE' , 'M' , 1000,'Y');
+('TANK-TOP' , 'BLACK' , 'S' , 1200,'Y');
+('TANK-TOP' , 'BLACK' , 'L' , 1300,'Y');
+('JEANS' , 'BLACK' , 'M' , 3000,'Y');
+('JEANS' , 'WHITE' , 'L' , 4000,'N');
+('JEANS' , 'BLUE' , 'L' , 2500,'Y');
+('TANK-TOP' , 'ORANGE' , 'M' , 2200,'Y');
+
+
+
+print all data using single line command
+
+add new shirt of grey color having size M and price 2000 which is available to sell
+
+select available articles and colors
+
+select all medium shirts
+
+select article,price of color white and size L 
+
+update all shirt size to L
+
+update price of the shirt to 0 if not available into the stock
+
+delete all articles if not available
+
+delete jeans whose price is less than 2500
+
+
+
+
+
 #joins
 #sql injection
 #SELECT * from EMPLOYEE where id=100 or   1=1  
