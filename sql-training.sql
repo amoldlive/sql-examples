421cde03afa2218a2d742c7d32b5d22f4a28a8f3
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-11-25 08:46:16 +0530

    initial commit
    initial commit

--- sql-training.sql
+++ sql-training.sql
@@ -1,19 +1,38 @@
 #Database Operation
 -------------------
-
 #create database
 #create database <database_name>;
+create database mydb;
 
+create database icici_db;
+
+create database student_db;
 
 #drop database 
 #drop database <database_name>
 
+drop database icici_db;
 
-#rename database name : Direct rename is not supported in mysl
+drop database student_db;
+
+#rename database name : Direct rename is not supported in mysql
 
 #select Databse
-#use database <database_name>
+#use <database_name>
+use mydb;
 
+#DDL
+#create table
+create table <tablename> (
+	// properties
+);
+
+create table person (
+  firstName varchar(100),
+  lastName  varchar(100),
+  age       int,
+  status    varchar(1)
+);
 
 #Data Types
 
@@ -63,8 +82,16 @@ boolean
 BOOL/boolean
 */
 
+#insert into <table_name> values();
+
+insert into person values('Virat','Kohli',38,'Y');
+
+#select * from <table_name>;
+
+select * from person;
+
 /*
-CRUD operaton
+CRUD operaton | DML
 --------------------
 CREATE READ UPDATE DELETE
 *
@@ -75,4 +102,28 @@ CREATE READ UPDATE DELETE
 **/
 
 
+#create Tabel
+
+/*
+
+create table <table_name>(
+	column_details
+);
+
+*/
+
+create table people(
+	column_details
+);
+
+create table employee(
+	id int,
+	name varchar(100),
+	address  varchar(100),
+	phone_number varchar(10),
+	email_id  varchar(100),
+	salary float
+);
+
+#Assignment-1 - create student table 
 
