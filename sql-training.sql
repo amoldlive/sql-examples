b14b2403db9bd7badb890965ab55b2b5b1d53bd3
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-11-30 08:41:28 +0530

    added String functions
    added String functions

--- sql-training.sql
+++ sql-training.sql
@@ -841,49 +841,196 @@ insert into person(firstName,lastName,age) values('Tejas','Varma',10);
 select * from person;
 
 
-#*****************Foreign Key ********************************
-#TODO : Understand Normalization and ACID Property
+#*****************String Function ********************************
 
-drop table vaccination;
+#refer belo link for more string function
+#https://dev.mysql.com/doc/refman/8.0/en/string-functions.html
 
-create table vaccination(
-	v_id int primary key auto_increment, 
-	v_name varchar(100),
-	v_entry_date datetime,
-	v_ex_date datetime
-);
+#Code formatter 
+#https://codebeautify.org/sqlformatter
 
-#SELECT NOW();
+create database booksdb;
 
-#select CURRENT_TIMESTAMP();
+use booksdb;
 
-
-insert into vaccination(v_name,v_entry_date,v_ex_date) values('Co-Vaxin',now() ,'2035-11-27 17:05:00');
-
-insert into vaccination(v_name,v_entry_date,v_ex_date) values('Covishield',now() ,'2035-11-27 17:05:00');
-
-insert into vaccination(v_name,v_entry_date,v_ex_date) values('?',now() ,'2035-11-27 17:05:00');
-
-select * from vaccination;
-
-drop table person;
-
-create table person(
-	id int primary key auto_increment ,
-	firstName varchar(20) not null,
-	lastName varchar(20) not null,
-	age int not null,
-	v_id int ,
-	foreign key(v_id) references vaccination(v_id)
-);
-
-
-insert into person (firstName ,lastName , age , v_id)
-values ('Rahul','Sharma',20,1);
-
-select * from person p ;
-
-insert into person (firstName ,lastName , age , v_id)
-values ('Rahul','Sharma',20,10);
-
+CREATE TABLE books 
+	(
+		book_id INT AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
  
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+VALUES
+('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+
+select * from books ;
+
+#concat
+select concat('A','B','C');
+
+SELECT CONCAT('pi', 'ckle');
+
+SELECT CONCAT(author_fname,' ', author_lname) as author_fullname  FROM books;
+
+SELECT CONCAT(author_fname,' ', author_lname) AS author_name FROM books;
+
+
+#concat_WS
+SELECT CONCAT_WS('-', 'I', 'am', 'learning', 'SQL!') as output;
+
+#I-am-learning-SQL!
+
+SELECT CONCAT_WS('-',title, author_fname, author_lname) FROM books;
+
+
+#substring
+
+/*
+sentence  			Hello World
+start point         1
+end point           4
+*/
+
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+
+SELECT SUBSTRING('Hello World', 9);
+ 
+#start with last location
+SELECT SUBSTRING('Hello World', -5 );
+ 
+
+SELECT title FROM books;
+
+#The Amazing Adventures of Kavalier & Clay : The Amazin
+
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books where title ='The Amazing Adventures of Kavalier & Clay';
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+
+
+#multi function
+
+select concat('Hello' ,'Wold')
+
+select substring('Hello Wold' , 7);
+
+select concat('Hello' ,substring('Hello Wold' , 7))
+
+select concat_ws('**','Hello' ,substring('Hello Wold' , 7))
+
+
+#The Amazin...
+
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+
+
+#Replace
+SELECT REPLACE('Hello World', 'Hell', '%$#@');
+ 
+SELECT REPLACE('Hello World', 'l', '7');
+ 
+SELECT REPLACE('Hello World', 'o', '0');
+ 
+SELECT REPLACE('HellO World', 'o', '*');
+ 
+select REPLACE('cheese bread coffee milk', ' ', ' and ');
+ 
+SELECT REPLACE(title, 'e ', '3') FROM books;
+ 
+SELECT REPLACE(title, ' ', '-') as newtitle FROM books;
+
+
+#reverse
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, '  ' ,REVERSE(author_fname)) FROM books;
+
+
+#length
+SELECT CHAR_LENGTH(' Hello World ');
+ 
+SELECT title , CHAR_LENGTH(title) as length  FROM books;
+ 
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+ 
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+
+#upper , lower
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
+
+
+#INSERT
+#Insert the string Replace the Nthe sixth characters:
+SELECT INSERT('Hello Bobby', 6, 0, 'There'); 
+
+SELECT INSERT('Hello Bobby', 6, 1, 'There'); 
+ 
+SELECT INSERT('Hello Bobby', 6, 2, 'There'); 
+
+#LEFT RIGHT REPEAT TRIM
+SELECT LEFT('omghahalol!', 3);
+ 
+SELECT RIGHT('omghahalol!', 4);
+ 
+SELECT REPEAT('ha', 4);
+ 
+SELECT TRIM('  pickle  ');
+
+
+#*****************String Function Excercise ********************************
+
+-- reverse and uppercase the following sentence
+ #A set of words that is complete in itself
+-- replace spaces in titles with ->
+-- print name and reverse of the name
+-- print book title and length of each title
+-- print short title , author , quantity #titlelength-> 10
+
+
+#*****************SQL Injection ********************************
+#SELECT * from EMPLOYEE where id=100 or   1=1  
