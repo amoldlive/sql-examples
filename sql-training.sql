1c80a050c89c91c19b0edfb4156540a0b2292c73
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-12-04 08:44:05 +0530

    updated inner join
    updated inner join

--- sql-training.sql
+++ sql-training.sql
@@ -1177,4 +1177,88 @@ SELECT * FROM orders WHERE customer_id = 1;
  
 SELECT * FROM orders 
 WHERE customer_id = (SELECT id FROM customers WHERE last_name = 'George');
+
  
+#*****************Join ********************************
+use mydb;
+
+drop table orders;
+drop table customers;
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+ 
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT
+);
+ 
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+     
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5),
+       ('1999-04-11', 450.25, 8);
+
+select * from customers c ;  
+select * from orders ; 
+
+-- To perform a (kind of useless) cross join:
+SELECT * FROM customers, orders;
+
+#*****************Inner Join ********************************
+SELECT * FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+SELECT * FROM customers
+INNER JOIN orders ON orders.customer_id = customers.id;
+ 
+
+SELECT first_name, last_name, order_date, amount FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+SELECT orders.id,customers.first_name, customers.last_name, orders.order_date, amount FROM customers
+JOIN orders ON orders.customer_id = customers.id;
+
+SELECT o.id,c.first_name, c.last_name, o.order_date, amount FROM customers c
+JOIN orders o ON o.customer_id = c.id;
+
+/*The order doesn't matter here*/
+
+SELECT * FROM orders
+JOIN customers ON customers.id = orders.customer_id;
+
+
+/*group By with join*/
+
+SELECT 
+    first_name, last_name, SUM(amount) AS total
+FROM
+    customers
+        JOIN
+    orders ON orders.customer_id = customers.id
+GROUP BY first_name , last_name;
+
+/*order By with join*/
+SELECT 
+    first_name, last_name, SUM(amount) AS total
+FROM
+    customers
+        JOIN
+    orders ON orders.customer_id = customers.id
+GROUP BY first_name , last_name
+order by total desc;
