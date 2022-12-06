4a1d096d5b92371184d11d2c36e999f7611c7dfa
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-12-06 08:01:06 +0530

    added cascade operation
    added cascade operation

--- sql-training.sql
+++ sql-training.sql
@@ -1327,8 +1327,10 @@ FROM
     orders ON customers.id = orders.customer_id ;
 	
 
--- On Delete Cascade
-	
+#*****************Cascade Operations ********************************
+drop table orders;
+drop table customers
+
 	CREATE TABLE customers (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50),
@@ -1346,6 +1348,24 @@ CREATE TABLE orders (
         ON DELETE CASCADE
 );
 
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+     
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
 
+ SELECT * FROM customers
+JOIN orders ON orders.customer_id = customers.id;
 
+delete from customers where id=1;
 
+ SELECT * FROM customers
+JOIN orders ON orders.customer_id = customers.id;
